81069e61e5aa94af87441876d1a2e4f7657782f2
Rodrigo <rplaza6692@gmail.com>
2023-01-15 18:45:48 -0300

    Actualizacion 15-ene
    Limpiamos un poco el código, suprimiendo carpetas y agregamos nuestra parte del backend para darle forma.

--- FrontEnd/portfolioplaza/src/app/app.module.ts
+++ FrontEnd/portfolioplaza/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoapComponent } from './components/logoap/logoap.component';
-import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -21,7 +20,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoapComponent,
-    SocialComponent,
     BannerComponent,
     AcercadeComponent,
     ExperienciaComponent,
